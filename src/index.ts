4a29da933948642eaa1befccef7c05ace64f7166
Kingston Duffie <kduffie@hivepoint.com>
2017-09-17 12:56:26 -0700

    no message

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ import { ExpressWithSockets, SocketConnectionHandler } from "./interfaces/expres
 import { socketServer } from "./socket-server";
 import { mediumManager } from "./medium-manager";
 
-const VERSION = 7;
+const VERSION = 8;
 const INITIAL_NETWORK_BALANCE = 25000;
 
 class ChannelsNetworkWebClient {
