af3b065bcbd9bbc8c083e3442b5554a554534be3
Kingston Duffie <kduffie@hivepoint.com>
2017-10-26 08:51:11 -0700

    no message

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ import { networkEntity } from "./network-entity";
 import { bank } from "./bank";
 import { emailManager } from "./email-manager";
 
-const VERSION = 30;
+const VERSION = 31;
 
 class ChannelsNetworkWebClient {
   private app: express.Application;
