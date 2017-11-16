5ae26185d3fb55cae71614ce6bb478ed2c58f321
Kingston Duffie <kduffie@hivepoint.com>
2017-11-16 15:57:58 -0800

    Fix 409 conflict on register

--- public/src/core/core.js
+++ public/src/core/core.js
@@ -73,14 +73,17 @@ class CoreService extends Polymer.Element {
     };
   }
 
-  ensureKey() {
+  ensureKey(forceNewKeys) {
     return new Promise((resolve, reject) => {
       this._loadKeyLib().then(() => {
-        if (this._keys && this._keys.privateKey) {
+        if (!forceNewKeys && this._keys && this._keys.privateKey) {
           resolve();
           return;
         }
         this._keys = $keyUtils.generateKey();
+        if (forceNewKeys) {
+          this.storage.clearItem(_CKeys.BACKUP_KEYS);
+        }
         this.storage.setItem(_CKeys.KEYS, this._keys, true);
         resolve();
       }).catch((err) => {
@@ -89,7 +92,7 @@ class CoreService extends Polymer.Element {
     });
   }
 
-  register(inviteCode) {
+  register(inviteCode, retrying) {
     // We want to avoid race conditions where multiple entities all ask to register
     // at the same time.  We only want to register once, so we return a promise for
     // others and resolve them after resolving the initial request.
@@ -128,8 +131,15 @@ class CoreService extends Polymer.Element {
       return info;
     }).catch((err) => {
       this._registrationInProgress = false;
-      this._resolvePendingRegistrations();
-      throw err;
+      if (err.code === 409 && !retrying) {
+        console.warn("core.register: received 409 conflict, retrying with fresh keys");
+        return this.ensureKey(true).then(() => {
+          return this.register(null, true);
+        });
+      } else {
+        this._resolvePendingRegistrations();
+        throw err;
+      }
     });
   }
 
