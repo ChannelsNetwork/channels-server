dc8f1a32127ccc81a6644170a0051df29380ee56
Kingston Duffie <kduffie@hivepoint.com>
2018-03-27 16:36:47 -0700

    Fix case where geo is null

--- src/card-manager.ts
+++ src/card-manager.ts
@@ -1032,7 +1032,7 @@ export class CardManager implements Initializable, NotificationHandler, CardHand
             amount: campaign.advertiserSubsidy,
             toRecipients: [subsidyRecipient],
           };
-          await networkEntity.performBankTransaction(request, requestBody.sessionId, details, card.summary.title, "Advertiser subsidy for pay-to-click card", geo.ipAddress, geo.fingerprint, now);
+          await networkEntity.performBankTransaction(request, requestBody.sessionId, details, card.summary.title, "Advertiser subsidy for pay-to-click card", geo ? geo.ipAddress : null, geo ? geo.fingerprint : null, now);
           console.log("Card.handleCardClicked: Paid advertiser subsidy", card.summary.title, card.by.handle, campaign.advertiserSubsidy, user.id, author.id);
         }
       }
@@ -1438,7 +1438,7 @@ export class CardManager implements Initializable, NotificationHandler, CardHand
           amount: campaign.advertiserSubsidy,
           toRecipients: [subsidyRecipient],
         };
-        await networkEntity.performBankTransaction(request, requestBody.sessionId, details, card.summary.title, "Advertiser subsidy for pay-to-open card", geo.ipAddress, geo.fingerprint, now);
+        await networkEntity.performBankTransaction(request, requestBody.sessionId, details, card.summary.title, "Advertiser subsidy for pay-to-open card", geo ? geo.ipAddress : null, geo ? geo.fingerprint : null, now);
         console.log("Card.handleRedeemCardOpen: Paid advertiser subsidy", card.summary.title, card.by.handle, campaign.advertiserSubsidy);
       }
       if (adSlot) {
